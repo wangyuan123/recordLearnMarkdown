bc9fb3630131bc876c986e68c03009935cb1e7ad
汪渊 <yuan.wang07@hand-china.com>
2022-09-22 17:41:05 +0800

    [ADD]: 工程初始化

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+const About = () => (
+  <div style={{ color: `teal` }}>
+      <Header siteTitle="拉布拉多汪汪汪" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
+export default About;
