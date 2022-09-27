3ba516bc0a615bf6e410a3ee3e684a46c7056cca
wangyuan <1105733477@qq.com>
2022-09-27 01:32:35 +0000

    deploy: 0167c6731040c1bca11d0a13ed55bf229e269f72

---
+++ component---src-pages-contact-js-e9f950bd002c6341b384.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[501],{5791:function(e,t,a){a.r(t);var n=a(7294),r=a(1597),l=a(2607);t.default=function(){return n.createElement("div",{style:{color:"teal"}},n.createElement(r.Link,{to:"/"},"Home"),n.createElement(l.Z,{headerText:"Contact"}),n.createElement("p",null,"Send us a message!"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-e9f950bd002c6341b384.js.map
